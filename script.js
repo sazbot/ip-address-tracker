5e95a164699b16bbfe65f9cce12c0e2ebef4eb35
Sarah Fielding Smith <s.fieldingsmith@gmail.com>
2022-08-13 13:57:07 +0100

    add marker to map

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
+// initialise leafletjs.com map
 let map = L.map("map").setView([51.505, -0.09], 13);
 
+// add tile layer
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   maxZoom: 19,
   attribution: "© OpenStreetMap",
 }).addTo(map);
+
+// add marker to map
+var marker = L.marker([51.5, -0.09]).addTo(map);
